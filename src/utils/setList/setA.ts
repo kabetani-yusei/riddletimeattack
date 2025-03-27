282a3f0455f1053119a3611a582ebd1dff336abc
kabetani-yusei <yuusei1771@gmail.com>
2025-03-27 22:37:52 +0900

    デプロイ

--- src/utils/setList/setA.ts
+++ src/utils/setList/setA.ts
@@ -1,30 +1,41 @@
 // src/constants/riddleSets/set1.ts
 import type { ImageSet } from "../types";
 
+import image1 from "../../assets/images/setA/image1.png";
+import image2 from "../../assets/images/setA/image2.png";
+import image3 from "../../assets/images/setA/image3.png";
+import image4 from "../../assets/images/setA/image4.png";
+import image5 from "../../assets/images/setA/image5.png";
+import image6 from "../../assets/images/setA/image6.png";
+import image7 from "../../assets/images/setA/image7.png";
+import image8 from "../../assets/images/setA/image8.png";
+import image9 from "../../assets/images/setA/image9.png";
+import image10 from "../../assets/images/setA/image10.png";
+
 export const setA: ImageSet = {
-	title: "Set A",
-	images: [
-		"src/assets/images/setA/image1.png",
-		"src/assets/images/setA/image2.png",
-		"src/assets/images/setA/image3.png",
-		"src/assets/images/setA/image4.png",
-		"src/assets/images/setA/image5.png",
-		"src/assets/images/setA/image6.png",
-		"src/assets/images/setA/image7.png",
-		"src/assets/images/setA/image8.png",
-		"src/assets/images/setA/image9.png",
-		"src/assets/images/setA/image10.png",
-	],
-	answers: [
-		"なぞなぞ",
-		"みしん",
-		"さんらん",
-		"かつじ",
-		"らんたん",
-		"ぷれい",
-		"ななもんめ",
-		"しゅぎょう",
-		"じんしゅ",
-		"しーる",
-	],
+  title: "Set A",
+  images: [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+  ],
+  answers: [
+    "なぞなぞ",
+    "みしん",
+    "さんらん",
+    "かつじ",
+    "らんたん",
+    "ぷれい",
+    "ななもんめ",
+    "しゅぎょう",
+    "じんしゅ",
+    "しーる",
+  ],
 };
--- src/utils/setList/setB.ts
+++ src/utils/setList/setB.ts
@@ -1,30 +1,41 @@
 // src/constants/riddleSets/set2.ts
-import { ImageSet } from "../types";
+import type { ImageSet } from "../types";
+
+import image1 from "../../assets/images/setB/image1.png";
+import image2 from "../../assets/images/setB/image2.png";
+import image3 from "../../assets/images/setB/image3.png";
+import image4 from "../../assets/images/setB/image4.png";
+import image5 from "../../assets/images/setB/image5.png";
+import image6 from "../../assets/images/setB/image6.png";
+import image7 from "../../assets/images/setB/image7.png";
+import image8 from "../../assets/images/setB/image8.png";
+import image9 from "../../assets/images/setB/image9.png";
+import image10 from "../../assets/images/setB/image10.png";
 
 export const setB: ImageSet = {
-	title: "Set B",
-	images: [
-		"src/assets/images/setB/image1.png",
-		"src/assets/images/setB/image2.png",
-		"src/assets/images/setB/image3.png",
-		"src/assets/images/setB/image4.png",
-		"src/assets/images/setB/image5.png",
-		"src/assets/images/setB/image6.png",
-		"src/assets/images/setB/image7.png",
-		"src/assets/images/setB/image8.png",
-		"src/assets/images/setB/image9.png",
-		"src/assets/images/setB/image10.png",
-	],
-	answers: [
-		"はじまり",
-		"ありさま",
-		"さやあて",
-		"まじっく",
-		"あさやけ",
-		"さっかく",
-		"かけじく",
-		"じさぼけ",
-		"まくあけ",
-		"くりあ",
-	],
+  title: "Set B",
+  images: [
+    image1,
+    image2,
+    image3,
+    image4,
+    image5,
+    image6,
+    image7,
+    image8,
+    image9,
+    image10,
+  ],
+  answers: [
+    "はじまり",
+    "ありさま",
+    "さやあて",
+    "まじっく",
+    "あさやけ",
+    "さっかく",
+    "かけじく",
+    "じさぼけ",
+    "まくあけ",
+    "くりあ",
+  ],
 };
